ffe7d42b3ebb064397bdc881cccbb7f395e86a5d
Ramon L. <ramon.2.fjlm@gmail.com>
2024-11-15 01:30:41 +0100

    Update: (Next output config)

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const isProduction = process.env.NODE_ENV === "production";
 const assetPrefix = isProduction ? "/home" : "";
 module.exports = {
 	...withNextra(),
+	output: 'export',
 	images: {
 		unoptimized: true,
 	},
