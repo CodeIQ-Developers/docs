8a94edf9d504e32f500b675564523e9dd734ac4b
Ramón López <ramon@masteritsupport.com>
2024-12-20 13:15:06 +0100

    .

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const isProduction = process.env.NODE_ENV === "production";
 const assetPrefix = isProduction ? "/home" : "";
 module.exports = {
 	...withNextra(),
-	// output: 'export',
+	output: 'export',
 	images: {
 		unoptimized: true,
 	},
